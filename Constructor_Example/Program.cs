5866a4d198750019b49196f5b255854a2f1d74d9
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:02:20 +0200

    Add project files.

---
+++ Constructor_Example/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Constructor_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DefConstructor dc = new DefConstructor();
+            dc.DoThis();
+            AddConstructor ac = new AddConstructor();
+            ac.DoThat();
+
+        }
+    }
+    public class DefConstructor
+    {
+        public void DoThis()
+        {
+            Console.WriteLine("DoThis has run...");
+        }
+
+    }
+    public class AddConstructor
+    {
+        public AddConstructor()
+        {
+            Console.WriteLine("the constructor has been called...");
+        }
+        public void DoThat()
+        {
+            Console.WriteLine("DoThat has has been executed...");
+        }
+
+    }
+
+
+
+}
